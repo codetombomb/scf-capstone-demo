0c2f8cca51009ac1f0e18e67a7495184b864b3ea
Tom Tobar <cocdetombomb@gmail.com>
2022-11-19 10:06:07 -0600

    add model relations

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,6 @@
 class Store < ApplicationRecord
+    has_many :deliveries
+    has_many :users, through: :deliveries
+
+    has_many :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :deliveries
+    has_many :stores, through: :deliveries
+
+    validates :email, :username, presence: true
 end

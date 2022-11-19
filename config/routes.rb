121dae23052e3374cc5237a905ad7b802bfc7e80
Tom Tobar <cocdetombomb@gmail.com>
2022-11-19 10:00:43 -0600

    add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  resources :orders
+  resources :deliveries
+  resources :stores
+  resources :users
   # route to test your configuration
   get '/hello', to: 'application#hello_world'
 end
